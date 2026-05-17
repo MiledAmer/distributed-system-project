b5fdb8fe235b7ed4b3332481b39d2e1e854e6a50
MiledAmer <midou.slm8@gmail.com>
2026-05-17 21:09:31 +0100

    main.js is done

--- orchestrator-service/src/main/java/org/project/Main.java
+++ orchestrator-service/src/main/java/org/project/Main.java
@@ -1,7 +1,38 @@
 package org.project;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public ManagedChannel orderChannel() {
+        return ManagedChannelBuilder
+                .forAddress("order-service", 50051)
+                .usePlaintext()
+                .build();
+    }
+
+    @Bean
+    public ManagedChannel kitchenChannel() {
+        return ManagedChannelBuilder
+                .forAddress("kitchen-service", 50052)
+                .usePlaintext()
+                .build();
+    }
+
+    @Bean
+    public ManagedChannel accountChannel() {
+        return ManagedChannelBuilder
+                .forAddress("account-service", 50053)
+                .usePlaintext()
+                .build();
     }
 }
